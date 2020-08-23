a80daa3d1ab4f4a2309b2f08d8d42610608c7fb1
mhanksid <adrianhartanto69@gmail.com>
2020-08-22 18:37:59 -0700

    910 Test

---
+++ precache-manifest.d14b5f757ec01378a9f70185099cd78f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f03c23737f448b589d5",
+    "url": "/910test/static/css/main.297203dc.chunk.css"
+  },
+  {
+    "revision": "2f03c23737f448b589d5",
+    "url": "/910test/static/js/main.2f03c237.chunk.js"
+  },
+  {
+    "revision": "ce4fe91a62b243f8114d",
+    "url": "/910test/static/js/runtime~main.ce4fe91a.js"
+  },
+  {
+    "revision": "39f9a0fb9bddb26d8ab0",
+    "url": "/910test/static/css/2.544ad022.chunk.css"
+  },
+  {
+    "revision": "39f9a0fb9bddb26d8ab0",
+    "url": "/910test/static/js/2.39f9a0fb.chunk.js"
+  },
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/910test/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "f391b5131b0d0a944a28c5b96b774e9a",
+    "url": "/910test/index.html"
+  }
+];
